9816254e2b594597f658c8130ed4a281ddef0bf4
Ali Jorgell Sequeira <alisequeira.la.240@gmail.com>
2020-08-12 14:14:34 -0400

    handlyn errors and conditionally render content

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -5,21 +5,29 @@ class App extends React.Component {
     constructor(props){
         super(props);
         //THIS IS THE ONLY TIME WE DO DIRECT  ASSIGMENT TO this.state
-        this.state = {latitude: null};
+        this.state = {latitude: null, errorMessage: ''};
 
         window.navigator.geolocation.getCurrentPosition(
             (position) =>{//To update state we call setState method!!!!!!!!!!
                 this.setState({latitude: position.coords.latitude})
                 //WE DID NOT this.state.latitude = position.coords.latitude;
             },
-            (err) => console.log(err)
+            (err) => this.setState({errorMessage: err.message})
         );
     }
-
+   
     //React says we have to define render!!
     render () {
-    
-        return <div>latitude: {this.state.latitude}</div>
+        if (this.state.latitude) {
+            //user allow geolocation
+            return <div> latitude: {this.state.latitude}</div>
+        }else if (this.state.errorMessage) {
+            //user denied geolocation
+            return <div> Error: {this.state.errorMessage}</div>
+        }else{
+            //waiting user responses 
+            return <div>Loading...</div>
+        }
     }
 }
 
