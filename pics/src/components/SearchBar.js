49468b9aa73d12cda142117cac1a0ec80371ba2f
Ali Jorgell Sequeira <alisequeira.la.240@gmail.com>
2020-08-13 11:27:23 -0400

    Adding a touch of style

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -7,8 +7,13 @@ class SearchBar extends React.Component {
 
     render(){
         return(
-            <div>
-                <input type="text"/>
+            <div className="ui segment">
+                <form className="ui form">
+                    <div className="field">
+                        <label>Image Search</label>
+                        <input type="text"/>
+                    </div>
+                </form>
             </div>
         );
     }
