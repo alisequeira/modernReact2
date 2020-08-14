3d0e586d9284d1d4ed601e99c014cbfb1a6a8f8a
Ali Jorgell Sequeira <alisequeira.la.240@gmail.com>
2020-08-14 12:45:31 -0400

    rendering video list

--- videos/src/components/VideoList.js
+++ videos/src/components/VideoList.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import VideoItem from './VideoItem';
 
  const VideoList = (props) => {
- return <div>{props.videos.length}</div>
+    const renderList = props.videos.map(video => {
+        return <VideoItem />
+    });
+ return <div>{renderList}</div>
 }
 
 export default VideoList;
