9b1127525d485ecbdecec4518f0ba406a5a35cc0
Ali Jorgell Sequeira <alisequeira.la.240@gmail.com>
2020-08-14 13:53:58 -0400

    fixing a few warning, adding grid layout

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -33,8 +33,16 @@ class App extends React.Component{
         return(
             <div className="ui container">
                 <SearchBar onFormSubmit={this.onTermSubmit}/>
-                <VideoDetail video={this.state.SelectedVideo} />
-               <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos}/>
+                <div className="ui grid">
+                    <div className = "ui row">
+                        <div className="eleven wide column">
+                            <VideoDetail video={this.state.SelectedVideo} />
+                        </div>
+                        <div className="five wide column">
+                            <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos}/>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
