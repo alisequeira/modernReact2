05defbef1171ac2a9e2fda5f69bf8e177a616740
Ali Jorgell Sequeira <alisequeira.la.240@gmail.com>
2020-08-14 13:34:24 -0400

    styling video detail

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import Youtube from '../apis/Youtube';
 import VideoList from './VideoList';
+import VideoDetail from './VideoDetail'
 
 const KEY = 'AIzaSyAW53-kovRD_1NUL-_XLUo6xeA2WLAWams';
 
@@ -25,13 +26,14 @@ class App extends React.Component{
       };
 
       onVideoSelect = video => {
-          console.log( 'from the app', video);
+         this.setState({SelectedVideo: video});
       }
 
     render(){
         return(
             <div className="ui container">
                 <SearchBar onFormSubmit={this.onTermSubmit}/>
+                <VideoDetail video={this.state.SelectedVideo} />
                <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos}/>
             </div>
         );
