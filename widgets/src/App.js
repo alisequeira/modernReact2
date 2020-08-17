b9a714c300da1031f764a37cd94e75eb3d403e81
Ali Jorgell Sequeira <alisequeira.la.240@gmail.com>
2020-08-17 11:37:52 -0400

    building and styling the accordion

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -3,15 +3,15 @@ import Accordion from './components/Accordion';
 
 const items =[
     {
-        tittle: 'What is React?',
+        title: 'What is React?',
         content: 'React is a front end JavaScript framework'
     },
     {
-        tittle: 'Why use React?',
+        title: 'Why use React?',
         content: 'React is a favorite JS library among engineers'
     },
     {
-        tittle: 'How do you use React',
+        title: 'How do you use React',
         content: 'You use React by creating components'
     }
 ];
