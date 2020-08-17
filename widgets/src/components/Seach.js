d32384925af8a6c909bac1b90ca028c984fc09e4
Ali Jorgell Sequeira <alisequeira.la.240@gmail.com>
2020-08-17 15:34:30 -0400

    default search terms

--- widgets/src/components/Seach.js
+++ widgets/src/components/Seach.js
@@ -1,10 +1,11 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 const Search = ()=> {
-    const [term , setTerm] = useState('');
+    const [term , setTerm] = useState('programing');
+    const [result, setResult] = useState([])
     useEffect(()=>{
         (async () => {
-            await axios.get('https://en.wikipedia.org/w/api.php',{
+          const{data} =  await axios.get('https://en.wikipedia.org/w/api.php',{
                 params: {
                     action: 'query',
                     list: 'search',
@@ -13,6 +14,7 @@ const Search = ()=> {
                     srsearch: term
                 }
             })
+            setResult(data);
         })();
     },[term]);
     return (
