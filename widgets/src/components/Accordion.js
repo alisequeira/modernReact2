ead30e1ab07c19812c3e15dcba1bc20521f6e897
Ali Jorgell Sequeira <alisequeira.la.240@gmail.com>
2020-08-17 12:33:54 -0400

    expanding the accordion

--- widgets/src/components/Accordion.js
+++ widgets/src/components/Accordion.js
@@ -8,14 +8,15 @@ const Accordion = ({items}) => {
     };
 
     const renderItems = items.map((item, index) => {
+        const active = index === activeIndex ? 'active' : ''
         return( <React.Fragment key={item.title}>
-                    <div className="title active"
+                    <div className={`title ${active}`}
                          onClick= {()=> onTitleClick(index)}  
                     >
                         <i className="dropdown icon"></i>
                         {item.title}
                     </div>
-                    <div className="content active">
+                    <div className={`content ${active}`}>
                         <p>{item.content}</p>
                     </div>
                 </React.Fragment>
@@ -25,7 +26,6 @@ return (
 
     <div className="ui styled accordion">
         {renderItems}
-        <h1>{activeIndex}</h1>
     </div>
 )
 };
