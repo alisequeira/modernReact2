1e8ced4c7470a48c25d48b93988e4b9cecf555b5
Ali Jorgell Sequeira <alisequeira.la.240@gmail.com>
2020-08-18 15:42:08 -0400

    detecting navigation

--- widgets/src/components/Route.js
+++ widgets/src/components/Route.js
@@ -1,6 +1,18 @@
-
+import {useState, useEffect} from 'react';
 const Route = ({path, children}) => {
+    useEffect(()=> {
+        const onLocationChange = () => {
+            console.log('on location change')
+        }
+        window.addEventListener('popstate', onLocationChange)
+
+        return () => {
+            window.removeEventListener('popstate', onLocationChange);
+        };
+    }, [])
     return window.location.pathname === path ? children : null;
 };
+
+
 
 export default Route;
