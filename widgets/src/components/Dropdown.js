3ff3e20147a5950d6bea9b9eb6e344650a5ff7b5
Ali Jorgell Sequeira <alisequeira.la.240@gmail.com>
2020-08-18 12:20:05 -0400

    body event listener clean up

--- widgets/src/components/Dropdown.js
+++ widgets/src/components/Dropdown.js
@@ -1,7 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 const Dropdown = ({options, selected , onSelectedChange}) => {
     const [open, setOpen] = useState(false)
+    const ref = useRef();
+
+    useEffect(() =>{
+        const onBodyClick = (event) => {
+            if(ref.current.contains(event.target)){
+                return;
+            }
+
+            setOpen(false);
+        }
+        document.body.addEventListener('click',onBodyClick);
+
+        return () => {
+            document.body.removeEventListener('click', onBodyClick);
+        };
+    },[])
     const renderedOptions = options.map((option)=> {
         if(option.value === selected.value){
             return null;
@@ -12,8 +28,9 @@ const Dropdown = ({options, selected , onSelectedChange}) => {
             </div>
         );
     });
+
     return (
-        <div className="ui form">
+        <div ref={ref} className="ui form">
             <div className="field">
                 <label className="label">Select a color</label>
                 <div onClick={()=> setOpen(!open)} className={`ui selection dropdown ${open ? 'visible active' : ''}`}>
