b6ea26d1e60ad9821c9cc88fab0694c112978bd4
Ali Jorgell Sequeira <alisequeira.la.240@gmail.com>
2020-08-19 15:05:33 -0400

    wiring up the provider

--- songs/src/reducers/index.js
+++ songs/src/reducers/index.js
@@ -1,3 +1,5 @@
+import {combineReducers} from 'redux'
+
 const songsReducer = () => {
     return [
         {title: 'Wind of Change', duration: '4:42'},
@@ -14,3 +16,8 @@ const selectedSongReducer = (selectedSong = null, action) => {
 
     return selectedSong;
 };
+
+export default combineReducers({
+    songs: songsReducer,
+    selectedSong: selectedSongReducer
+});
