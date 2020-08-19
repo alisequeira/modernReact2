31bf1afc58c2ac168f1e0650c3da422c745c94ad
Ali Jorgell Sequeira <alisequeira.la.240@gmail.com>
2020-08-19 17:17:31 -0400

    conditional rendering

--- songs/src/components/SongDetail.js
+++ songs/src/components/SongDetail.js
@@ -2,13 +2,28 @@ import React from 'react';
 import {connect} from 'react-redux';
 
 
-const SongDetail = (props)  => {
-    console.log(props)
-    return <div>Song Detail</div>
+const SongDetail = ({selectedSong})  => {
+    
+    if(!selectedSong){
+        return <div> Select a song</div>
+    }
+    let type= selectedSong.hasOwnProperty('band') ? 'band' : 'singer';
+    return (
+                <div>
+                    <h3>Details For:</h3>
+                    <p>
+                       Title: {selectedSong.title}
+                       <br/>
+                       Duration: {selectedSong.duration}
+                       <br/>
+                       {`${type}: ${selectedSong[type]}`}
+                    </p>
+                </div>
+            )
 };
 
 const mapStateToProps = (state) => {
-    return {song: state.selectedSong}
+    return {selectedSong: state.selectedSong}
 };
 
 export default connect(mapStateToProps)(SongDetail);
--- songs/src/reducers/index.js
+++ songs/src/reducers/index.js
@@ -2,10 +2,10 @@ import {combineReducers} from 'redux'
 
 const songsReducer = () => {
     return [
-        {title: 'Wind of Change', duration: '4:42'},
-        {title: 'Paranoid', duration: '2:52'},
-        {title: 'Crazy Train', duration: '4:57'},
-        {title: 'have you ever seen the rain', duration: '2:47'}
+        {title: 'Wind of Change',band: 'Scorpions', duration: '4:42'},
+        {title: 'Paranoid', band: 'Black Sabbath', duration: '2:52'},
+        {title: 'Crazy Train', singer: 'Ozzy Osburne', duration: '4:57'},
+        {title: 'have you ever seen the rain', band: 'Creedence Clearwater Revival', duration: '2:47'}
     ];
 };
 
