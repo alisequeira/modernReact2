576478333e6a48786fdad09c4b1aefc8c843dce0
Ali Jorgell Sequeira <alisequeira.la.240@gmail.com>
2020-08-19 14:06:50 -0400

    building reducers

---
+++ songs/src/reducers/index.js
@@ -0,0 +1,8 @@
+const songsReducer = () => {
+    return [
+        {title: 'Wind of Change', duration: '4:42'},
+        {title: 'Paranoid', duration: '2:52'},
+        {title: 'Crazy Train', duration: '4:57'},
+        {title: 'have you ever seen the rain', duration: '2:47'}
+    ];
+};
