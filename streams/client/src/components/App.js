0b00d2e8e611d6154efe5f152f6a8364744b6d9e
Ali Jorgell Sequeira <alisequeira.la.240@gmail.com>
2020-08-21 13:45:51 -0400

    connecting the header

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {BrowserRouter, Route, Link} from 'react-router-dom';
+import Header from './Header';
 import StreamCreate from './streams/StreamCreate';
 import StreamDelete from './streams/StreamDelete';
 import StreamEdit from './streams/StreamEdit';
@@ -13,7 +14,8 @@ import StreamShow from './streams/StreamShow';
 
 const App = () => {
     return (
-        <div>
+        <div className="ui container">
+            <Header/>
             <BrowserRouter>
                 <div>
                     <Route path="/" exact component={StreamList}/>
