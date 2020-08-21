b0b7a24042625eedf3b9b8070bd4142c8c00bfeb
Ali Jorgell Sequeira <alisequeira.la.240@gmail.com>
2020-08-21 13:27:45 -0400

    wiring up routes

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,36 +1,26 @@
 import React from 'react';
 import {BrowserRouter, Route, Link} from 'react-router-dom';
+import StreamCreate from './streams/StreamCreate';
+import StreamDelete from './streams/StreamDelete';
+import StreamEdit from './streams/StreamEdit';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
 /*
     Link: Is  a react component that we are goint to use in place of any anchor tags(helps to navigate single page application)
  */
 
-const pageOne = () => {
-    return(
-        <div>
-            Page One 
-        <Link to="/pageTwo">Navigate to Page Two</Link>
-        </div>
-    );
-};
 
-const pageTwo = () => {
-    return(
-        <div>
-            Page Two
-        <button>Click Me</button>
-
-        <Link to="/"> Navigate to Page One</Link>
-        </div>
-    );
-};
 
 const App = () => {
     return (
         <div>
             <BrowserRouter>
                 <div>
-                <Route path="/" exact component={pageOne} />
-                <Route path="/pagetwo"  component={pageTwo} />
+                    <Route path="/" exact component={StreamList}/>
+                    <Route path="/streams/new" exact component={StreamCreate}/>
+                    <Route path="/streams/edit" exact component={StreamEdit}/>
+                    <Route path="/streams/delete" exact component={StreamDelete}/>
+                    <Route path="/streams/Show" exact component={StreamShow}/>
                 </div>
             </BrowserRouter>
         </div>
