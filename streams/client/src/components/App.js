efadc319ce664969d57b7e5d9e132593e027d1d5
Ali Jorgell Sequeira <alisequeira.la.240@gmail.com>
2020-08-21 13:00:31 -0400

    introducing react router

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,7 +1,40 @@
 import React from 'react';
+import {BrowserRouter, Route, Link} from 'react-router-dom';
+/*
+    Link: Is  a react component that we are goint to use in place of any anchor tags(helps to navigate single page application)
+ */
+
+const pageOne = () => {
+    return(
+        <div>
+            Page One 
+        <Link to="/pageTwo">Navigate to Page Two</Link>
+        </div>
+    );
+};
+
+const pageTwo = () => {
+    return(
+        <div>
+            Page Two
+        <button>Click Me</button>
+
+        <Link to="/"> Navigate to Page One</Link>
+        </div>
+    );
+};
 
 const App = () => {
-    return <div>App</div>
+    return (
+        <div>
+            <BrowserRouter>
+                <div>
+                <Route path="/" exact component={pageOne} />
+                <Route path="/pagetwo"  component={pageTwo} />
+                </div>
+            </BrowserRouter>
+        </div>
+    );
 };
 
 export default App;
