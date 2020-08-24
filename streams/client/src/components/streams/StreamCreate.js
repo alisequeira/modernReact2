6812be8cbbcebbc079f6740bac3852fce9aa3a5b
Ali Jorgell Sequeira <alisequeira.la.240@gmail.com>
2020-08-24 15:47:59 -0400

    creating form

--- streams/client/src/components/streams/StreamCreate.js
+++ streams/client/src/components/streams/StreamCreate.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import {Field, reduxForm} from 'redux-form';//Field is a react component and reduxForm is a function
+//this component was refactor to a class base component cuz we will have a bunch of helper methods so \
+//we can kind of better organize our code.
 
-const StreamCreate = () => {
-    return <div>StreamCreate</div>
-};
+class StreamCreate extends React.Component {
+    render() {
+        return (
+            <form>
+                <Field name="title"/>
+                <Field name="description"/>
+            </form>
+        )
+    }
+}
 
-export default StreamCreate;
+export default reduxForm({
+    form:'streamCreate'
+})(StreamCreate);
