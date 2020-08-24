bc05fd1c0b67a0bbd795be5dd716049f01bc55e6
Ali Jorgell Sequeira <alisequeira.la.240@gmail.com>
2020-08-24 15:21:19 -0400

    connecting redux form

--- streams/client/src/reducers/index.js
+++ streams/client/src/reducers/index.js
@@ -1,6 +1,10 @@
 import {combineReducers} from 'redux';
+import {reducer as formReducer } from 'redux-form';
+/*renamed the reduce to not confuse */
+//we have tu pull this reducer out of the library and hook it up to our combined reducer
 import authReducer from './authReducer';
 
 export default combineReducers({
-    auth: authReducer
+    auth: authReducer,
+    form: formReducer
 });
