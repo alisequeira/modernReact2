99999042085207171330400fefd5bdeff9326f4f
Ali Jorgell Sequeira <alisequeira.la.240@gmail.com>
2020-08-25 13:22:28 -0400

    handling fetching, creating and updating

---
+++ streams/client/src/reducers/streamReducer.js
@@ -0,0 +1,15 @@
+import { act } from "react-dom/test-utils";
+
+export default (state = {}, action) => {
+    switch(action.type){
+        case 'FETCH_STREAM':
+            return {...state, [action.payload.id]: action.payload};
+        
+        case 'CREATE_STREAM': 
+        return {...state, [action.payload.id]: action.payload};
+
+        case 'EDIT_STREAM':
+            return {...state, [action.payload.id]: action.payload};
+        default: return state;
+    }
+}
