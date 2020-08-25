7962cbd9a6da7106d62a31731bc6151a9c2c0264
Ali Jorgell Sequeira <alisequeira.la.240@gmail.com>
2020-08-25 17:31:39 -0400

    refactoring stream creation

--- streams/client/src/components/streams/StreamCreate.js
+++ streams/client/src/components/streams/StreamCreate.js
@@ -1,69 +1,23 @@
 import React from 'react';
-import {Field, reduxForm} from 'redux-form';//Field is a react component and reduxForm is a function
-//this component was refactor to a class base component cuz we will have a bunch of helper methods so \
-//we can kind of better organize our code.\
 import {connect} from 'react-redux';
 import {createStream} from '../../actions';
+import StreamForm from './StreamForm';
 
 class StreamCreate extends React.Component {
-    renderError = ({error, touched})=> {
-        if(touched && error) {
-            return (
-                <div className="ui error message">
-                    <div className="header">
-                        {error}
-                    </div>
-                </div>
-            )
-        }
-    }
-    renderInput = (formProps) => {
-        //is gona take all the input properties out there and add them as props
-        const className = `field ${formProps.meta.error && formProps.meta.touched ? 'error' : ''}`;
-        return (
-                    <div className={className}>
-                        <label>{formProps.label}</label>
-                        <input {...formProps.input}/>
-                        {this.renderError(formProps.meta)}
-                    </div>
-                )
-    }
-
     onSubmit = (formValues) => {
 //doing this is going to be easier for us take those form values now and save them on some api or validate or whatever it's we want to do with
         this.props.createStream(formValues);
     }
 
     render() {
-        //prop component is going to be a real component or a function for the field to call
-        //this function or component need to return some element is going to be actually shown on the screen
         return (
-            <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form error">
-                <Field name="title" component={this.renderInput} label="Enter title"/>
-                <Field name="description" component={this.renderInput} label="Enter description"/>
-                <button className="ui button primary">Submit</button>
-            </form>
+         <div>
+             <h3>Create a stream</h3>
+             <StreamForm onSubmit={this.onSubmit}/>
+         </div>
         )
     }
 }
 
-const validate = (formValues) => {
-    const errors = {};
 
-    if(!formValues.title){
-        errors.title = 'You must enter a title'
-    }
-
-    if(!formValues.description){
-        errors.description = 'You must a description'
-    }
-
-    return errors;
-}
-
-const formWrapped = reduxForm({
-    form:'streamCreate',
-    validate
-})(StreamCreate);
-
-export default connect(null, ({createStream})) (formWrapped);
+export default connect(null, ({createStream})) (StreamCreate);
---
+++ streams/client/src/components/streams/StreamForm.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import {Field, reduxForm} from 'redux-form';//Field is a react component and reduxForm is a function
+//this component was refactor to a class base component cuz we will have a bunch of helper methods so \
+//we can kind of better organize our code.\
+
+class StreamForm extends React.Component {
+    renderError = ({error, touched})=> {
+        if(touched && error) {
+            return (
+                <div className="ui error message">
+                    <div className="header">
+                        {error}
+                    </div>
+                </div>
+            )
+        }
+    }
+    renderInput = (formProps) => {
+        //is gona take all the input properties out there and add them as props
+        const className = `field ${formProps.meta.error && formProps.meta.touched ? 'error' : ''}`;
+        return (
+                    <div className={className}>
+                        <label>{formProps.label}</label>
+                        <input {...formProps.input}/>
+                        {this.renderError(formProps.meta)}
+                    </div>
+                )
+    }
+
+    onSubmit = (formValues) => {
+//doing this is going to be easier for us take those form values now and save them on some api or validate or whatever it's we want to do with
+        this.props.onSubmit(formValues);
+    }
+
+    render() {
+        //prop component is going to be a real component or a function for the field to call
+        //this function or component need to return some element is going to be actually shown on the screen
+        return (
+            <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form error">
+                <Field name="title" component={this.renderInput} label="Enter title"/>
+                <Field name="description" component={this.renderInput} label="Enter description"/>
+                <button className="ui button primary">Submit</button>
+            </form>
+        )
+    }
+}
+
+const validate = (formValues) => {
+    const errors = {};
+
+    if(!formValues.title){
+        errors.title = 'You must enter a title'
+    }
+
+    if(!formValues.description){
+        errors.description = 'You must a description'
+    }
+
+    return errors;
+}
+
+export default reduxForm({
+    form:'streamForm',
+    validate
+})(StreamForm);
