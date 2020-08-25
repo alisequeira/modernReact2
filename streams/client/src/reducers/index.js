0a3cad3149c007c50a0cbe1da7c76232ca109fc7
Ali Jorgell Sequeira <alisequeira.la.240@gmail.com>
2020-08-25 13:41:38 -0400

    mergin list of records

--- streams/client/src/reducers/index.js
+++ streams/client/src/reducers/index.js
@@ -3,8 +3,10 @@ import {reducer as formReducer } from 'redux-form';
 /*renamed the reduce to not confuse */
 //we have tu pull this reducer out of the library and hook it up to our combined reducer
 import authReducer from './authReducer';
+import streamReducer from './streamReducer';
 
 export default combineReducers({
     auth: authReducer,
-    form: formReducer
+    form: formReducer,
+    streams: streamReducer
 });
