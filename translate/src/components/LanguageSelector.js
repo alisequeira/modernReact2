a6a143e8240273046999013bc7c83c7eaeb6785e
Ali Jorgell Sequeira <alisequeira.la.240@gmail.com>
2020-08-27 15:40:04 -0400

    connecting the selector to the store

--- translate/src/components/LanguageSelector.js
+++ translate/src/components/LanguageSelector.js
@@ -1,14 +1,14 @@
 import React from 'react';
-
+import LanguageContext from '../contexs/LanguageContext'
 class LanguageSelector extends React.Component{
 
-
+    static contextType = LanguageContext;
     render(){
         return(
             <div>
                     Select a language: 
-                    <i className="flag us" onClick={()=> this.props.onLanguageChange('english')}/>
-                    <i className=" flag ni" onClick={()=> this.props.onLanguageChange('spanish')}/>
+                    <i className="flag us" onClick={()=> this.context.onLanguageChange('english')}/>
+                    <i className=" flag ni" onClick={()=> this.context.onLanguageChange('spanish')}/>
                 </div>
         )
     }
