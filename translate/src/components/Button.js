94947281f6791bffc265f6ce50986a50ba0d1e50
Ali Jorgell Sequeira <alisequeira.la.240@gmail.com>
2020-08-27 13:55:09 -0400

    accessing data with consumer

--- translate/src/components/Button.js
+++ translate/src/components/Button.js
@@ -2,11 +2,17 @@ import React from 'react';
 import LanguageContext from '../contexs/LanguageContext';
 
 class Button extends React.Component {
-//1. set up context type property(this step is what forms the link between our class and the context object)
-    static contextType = LanguageContext;//similar to Button.contextType = LanguageContext(but you must do it after the class)
+//when we are using a consumer we dont have to specify a context type. context type is only required 
+// when we want to get our data assigned to destock context property
     render(){
-        const text = this.context === 'english' ? 'submit' : 'Enviar'; 
-        return <button className="ui button primary">{text}</button>;
+        return <button className="ui button primary">
+            <LanguageContext.Consumer>
+                {/* Any time we make use of a consumer to get a value out of that pipe pr that context object
+                    we are gonna pass in a single function as a child, that will be called with whatever value
+                    is inside of our pipe so we can implement some logic inside */}
+                {(value) => value === 'english' ? 'Submit' : 'Enviar'}
+            </LanguageContext.Consumer>
+        </button>;
     }
 }
 export default Button;
--- translate/src/components/Field.js
+++ translate/src/components/Field.js
@@ -2,8 +2,8 @@ import React from 'react';
 import LanguageContext from '../contexs/LanguageContext';
 
 class Field extends React.Component{
-
-    static contextType = LanguageContext;
+//1. set up context type property(this step is what forms the link between our class and the context object)
+    static contextType = LanguageContext;//similar to Field.contextType = LanguageContext(but you must do it after the class)
 
     render(){
         const text = this.context === 'english' ? 'Name' : 'Nombre';
